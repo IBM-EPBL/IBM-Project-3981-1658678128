f8f3be8528bbe4a9d3501824c0b947bee5573287
Jumli Kamki <103740530+jumliortps@users.noreply.github.com>
2022-09-24 21:15:56 +0530

    Update GCTC1917124.py

--- Assignments/Team Lead - Jumli Kamki/Assignment 2/GCTC1917124.py
+++ Assignments/Team Lead - Jumli Kamki/Assignment 2/GCTC1917124.py
@@ -1,7 +1,7 @@
 import random
 
 while(True):
-    temp = random.random()
-    hum = random.random()
-    if(temp*100>57):
-        print("There's a fire!!!")
+    temp = random.random() #getting temperature value
+    hum = random.random() #getting humidity value
+    if(temp*100>57): #checking if the temperature is high enough for a fire 
+        print("There's a fire!!!") #setting off the alarm
