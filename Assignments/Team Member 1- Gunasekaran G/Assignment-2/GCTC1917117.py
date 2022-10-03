cce1a2ac0e8b352ca6153c45f4a5871647952178
Gunasekaran567 <114922919+Gunasekaran567@users.noreply.github.com>
2022-10-03 18:30:57 +0530

    Add files via upload

---
+++ Assignments/Team Member 1- Gunasekaran G/Assignment-2/GCTC1917117.py
@@ -0,0 +1,9 @@
+​import​ ​random
+ 
+ ​while​(​True​):
+ 
+ ​    ​hum​ ​=​ ​random​.​random​() ​#getting humidity value
+ 
+ ​        ​if​(​hum​*​100​>​70​): ​#checking if the humidity is high enough for smoke from a fire
+ 
+ ​            ​print​(​"There's a fire!!!"​) ​#setting off the alarm
