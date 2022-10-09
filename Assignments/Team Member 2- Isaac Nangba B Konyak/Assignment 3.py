9b80567994526b77d7352867d82980086f1fde6c
isaacnangba <103741966+isaacnangba@users.noreply.github.com>
2022-10-09 14:50:33 +0530

    Create Assignment 3.py

---
+++ Assignments/Team Member 2- Isaac Nangba B Konyak/Assignment 3.py
@@ -0,0 +1,21 @@
+from gpiozero import TrafficLights
+
+from time import sleep
+
+lights = TrafficLights(2, 3, 4) #Pins 
+
+lights.green.on()
+
+while True:
+   sleep(10)
+   lights.amber.on()
+   lights.green.off()
+   sleep(1)
+   lights.red.on()
+   lights.amber.off()
+   sleep(10)
+   lights.amber.on()
+   lights.red.off()
+   sleep(1)
+   lights.green.on()
+   lights.amber.off()
